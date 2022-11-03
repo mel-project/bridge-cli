690255045957c3e0aa5800e14f0d2cb1fa7407e6
sadministrator <marcothewebdev@gmail.com>
2022-11-02 23:57:26 -0600

    Intial refactors to argh.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 
-use clap::Parser;
+use argh::FromArgs;
 use themelio_structs::{
     CoinID,
     CoinData,
@@ -8,40 +8,70 @@ use themelio_structs::{
     Transaction,
 };
 
-#[derive(Parser, Debug)]
-#[clap(author, version, about, long_about = None)]
-struct Args {
-    #[clap(short, long)]
+#[derive(FromArgs, PartialEq, Debug)]
+struct TopLevel {
+    #[argh(subcommand)]
+    sequence_type: TxSequenceType,
+}
+
+#[derive(FromArgs, PartialEq, Debug)]
+#[argh(subcommand)]
+enum TxSequenceType {
+    FreezeAndMint(FreezeAndMintArgs),
+    BurnAndThaw(BurnAndThawArgs),
+}
+
+#[derive(FromArgs)]
+#[argh(subcommand, name = "freeze_and_mint_args", description = "arguments for coin freezing and token minting transactions (i.e. bridging from Themelio to Ethereum)")]
+struct FreezeAndMintArgs {
+    #[argh(option)]
+    tx_args: TransactionArgs,
+}
+
+#[derive(FromArgs)]
+#[argh(subcommand, name = "burn_and_thaw_args", description = "arguments for token burning and coin thawing transaction (i.e. bridging back from Ethereum to Themelio)")]
+struct BurnAndThawArgs{
+    #[argh(option)]
+    tx_args: TransactionArgs,
+}
+
+#[derive(FromArgs)]
+#[argh(description = "arguments for a transaction: value, denom, and Ethereum recipient")]
+struct TransactionArgs {
+    #[argh(option, short = 'v', description = "value of the transaction")]
     value: String,
 
-    #[clap(short, long)]
+    #[argh(option, short = 'd', description = "denom of the transaction")]
     denom: String,
 
-    #[clap(short, long)]
+    #[argh(option, short = 'r', description = "ethereum address of recipient")]
     eth_recipient: String,
 }
 
 fn main() {
-    let args = Args::parse();
+    let args: TopLevel = argh::from_env();
 
-    let value: u128 = args
-        .value
-        .parse()
-        .expect("Please include coin value.");
+    match args {
+        
+    }
+    // let value: u128 = args
+    //     .value
+    //     .parse()
+    //     .expect("Please include coin value.");
 
-    let denom: Denom = args
-        .denom
-        .parse()
-        .expect("Please include coin denom.");
+    // let denom: Denom = args
+    //     .denom
+    //     .parse()
+    //     .expect("Please include coin denom.");
 
-    let eth_recipient_str: &str = args
-        .eth_recipient
-        .strip_prefix("0x")
-        .expect("Address must start with '0x'.");
-    let eth_recipient: [u8; 20] = hex::decode(eth_recipient_str)
-        .expect("Error decoding Ethereum recipient address.")
-        .try_into()
-        .expect("Recipient address must be 20 bytes long.");
+    // let eth_recipient_str: &str = args
+    //     .eth_recipient
+    //     .strip_prefix("0x")
+    //     .expect("Address must start with '0x'.");
+    // let eth_recipient: [u8; 20] = hex::decode(eth_recipient_str)
+    //     .expect("Error decoding Ethereum recipient address.")
+    //     .try_into()
+    //     .expect("Recipient address must be 20 bytes long.");
 
     println!("value: {}\ndenom: {}\nrecipient: {}", value, denom, hex::encode(eth_recipient));
 }
