86c621689354868b51477bc3b8b04d0244065cd0
sadministrator <marcothewebdev@gmail.com>
2022-11-03 00:27:35 -0600

    Better structure to arguments.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::fmt;
 
 use argh::FromArgs;
 use themelio_structs::{
@@ -9,69 +10,52 @@ use themelio_structs::{
 };
 
 #[derive(FromArgs, PartialEq, Debug)]
+#[argh(description = "top-level cli argument; an enum of subcommands")]
 struct TopLevel {
     #[argh(subcommand)]
-    sequence_type: TxSequenceType,
+    subcommand: Subcommand,
 }
 
 #[derive(FromArgs, PartialEq, Debug)]
 #[argh(subcommand)]
-enum TxSequenceType {
+enum Subcommand {
     FreezeAndMint(FreezeAndMintArgs),
     BurnAndThaw(BurnAndThawArgs),
 }
 
-#[derive(FromArgs)]
-#[argh(subcommand, name = "freeze_and_mint_args", description = "arguments for coin freezing and token minting transactions (i.e. bridging from Themelio to Ethereum)")]
+#[derive(FromArgs, PartialEq, Debug)]
+#[argh(subcommand, name = "freeze_and_mint", description = "arguments for coin freezing and token minting transactions (i.e. bridging from Themelio to Ethereum)")]
 struct FreezeAndMintArgs {
-    #[argh(option)]
-    tx_args: TransactionArgs,
-}
-
-#[derive(FromArgs)]
-#[argh(subcommand, name = "burn_and_thaw_args", description = "arguments for token burning and coin thawing transaction (i.e. bridging back from Ethereum to Themelio)")]
-struct BurnAndThawArgs{
-    #[argh(option)]
-    tx_args: TransactionArgs,
-}
-
-#[derive(FromArgs)]
-#[argh(description = "arguments for a transaction: value, denom, and Ethereum recipient")]
-struct TransactionArgs {
     #[argh(option, short = 'v', description = "value of the transaction")]
     value: String,
 
     #[argh(option, short = 'd', description = "denom of the transaction")]
     denom: String,
 
-    #[argh(option, short = 'r', description = "ethereum address of recipient")]
-    eth_recipient: String,
+    #[argh(option, short = 'e', description = "ethereum address of recipient")]
+    ethereum_recipient: String,
 }
 
+#[derive(FromArgs, PartialEq, Debug)]
+#[argh(subcommand, name = "burn_and_thaw", description = "arguments for token burning and coin thawing transaction (i.e. bridging back from Ethereum to Themelio)")]
+struct BurnAndThawArgs{
+    #[argh(option, short = 'v', description = "value of the transaction")]
+    value: String,
+
+    #[argh(option, short = 'd', description = "denom of the transaction")]
+    denom: String,
+
+    #[argh(option, short = 't', description = "themelio address of recipient")]
+    themelio_recipient: String,
+}
+
+
+
 fn main() {
-    let args: TopLevel = argh::from_env();
+    let top_level: TopLevel = argh::from_env();
 
-    match args {
-        
+    match top_level.subcommand {
+        Subcommand::FreezeAndMint(args) => println!("{:?}", args),
+        Subcommand::BurnAndThaw(args) => println!("{:?}", args),
     }
-    // let value: u128 = args
-    //     .value
-    //     .parse()
-    //     .expect("Please include coin value.");
-
-    // let denom: Denom = args
-    //     .denom
-    //     .parse()
-    //     .expect("Please include coin denom.");
-
-    // let eth_recipient_str: &str = args
-    //     .eth_recipient
-    //     .strip_prefix("0x")
-    //     .expect("Address must start with '0x'.");
-    // let eth_recipient: [u8; 20] = hex::decode(eth_recipient_str)
-    //     .expect("Error decoding Ethereum recipient address.")
-    //     .try_into()
-    //     .expect("Recipient address must be 20 bytes long.");
-
-    println!("value: {}\ndenom: {}\nrecipient: {}", value, denom, hex::encode(eth_recipient));
 }
