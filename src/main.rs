7f11784b8f32ddefa60d7fd1818cb9fc389047ee
sadministrator <marcothewebdev@gmail.com>
2022-11-03 15:23:33 -0600

    Implemented covenant compilation from submodule.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,20 @@
-use std::fmt;
+use std::path::Path;
 
+use anyhow::Result;
 use argh::FromArgs;
-use themelio_structs::{
-    CoinID,
-    CoinData,
-    Denom,
-    Header,
-    Transaction,
-};
+use melorun::LoadFileError;
+use mil::compiler::{BinCode, Compile};
+use themelio_stf::melvm::Covenant;
+// use themelio_structs::{
+//     CoinID,
+//     CoinData,
+//     Denom,
+//     Header,
+//     Transaction,
+// };
 
 #[derive(FromArgs, PartialEq, Debug)]
-#[argh(description = "top-level cli argument; an enum of subcommands")]
+#[argh(description = "top-level cli argument")]
 struct TopLevel {
     #[argh(subcommand)]
     subcommand: Subcommand,
@@ -37,7 +41,7 @@ struct FreezeAndMintArgs {
 }
 
 #[derive(FromArgs, PartialEq, Debug)]
-#[argh(subcommand, name = "burn_and_thaw", description = "arguments for token burning and coin thawing transaction (i.e. bridging back from Ethereum to Themelio)")]
+#[argh(subcommand, name = "burn_and_thaw", description = "arguments for token burning and coin thawing transactions (i.e. bridging back from Ethereum to Themelio)")]
 struct BurnAndThawArgs{
     #[argh(option, short = 'v', description = "value of the transaction")]
     value: String,
@@ -49,13 +53,36 @@ struct BurnAndThawArgs{
     themelio_recipient: String,
 }
 
+const COV_PATH: &str = "bridge-covenants/bridge.melo";
 
+fn compile() -> Result<Covenant> {
+    let cov_path = Path::new(COV_PATH);
+    let melo_str = std::fs::read_to_string(cov_path)
+        .map_err(LoadFileError::IoError)?;
+    let (s, _) = melodeon::compile(&melo_str, cov_path)
+        .map_err(LoadFileError::MeloError)?;
+    let parsed = mil::parser::parse_no_optimize(&s)
+        .expect("BUG: mil compilation failed");
+    let melvm_ops = parsed
+        .compile_onto(BinCode::default())
+        .0;
+    let covenant = Covenant::from_ops(&melvm_ops)?;
 
-fn main() {
+    Ok(covenant)
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
     let top_level: TopLevel = argh::from_env();
 
     match top_level.subcommand {
         Subcommand::FreezeAndMint(args) => println!("{:?}", args),
         Subcommand::BurnAndThaw(args) => println!("{:?}", args),
     }
+
+    let cov = compile()?;
+
+    println!("{:?}", cov);
+
+    Ok(())
 }
